4800c71d4dcc6c5889676d10ac8b057cbd2a329f
C.H. Hsiao <hsiaosvideo@gmail.com>
2024-08-03 00:40:35 +0800

    Remove the Dbl and Rdbl signs; add two new table styles in competitive bidding

--- replace_suit_signs.js
+++ replace_suit_signs.js
@@ -1,8 +1,6 @@
 function replaceSuitSigns(tag) {
     for (let i = 0; i < document.getElementsByTagName(tag).length; i++) {
         let text = document.getElementsByTagName(tag)[i].innerHTML;
-        text = text.replace(/Dbl./g, "<span style=\"font-weight: bold; color:red;\">X</span>");
-        text = text.replace(/Rdbl./g, "<span style=\"font-weight: bold; color:blue;\">XX</span>");
         text = text.replace(/!H/g, "<span style=\"color:tomato\">&#9829;</span>");
         text = text.replace(/!D/g, "<span style=\"color:orange\">&#9830;</span>");
         text = text.replace(/!C/g, "<span style=\"color:DarkSlateGray\">&#9827;</span>");
