8924f7d3c20c371501776962666302c137e5a886
C.H. Hsiao <hsiaosvideo@gmail.com>
2024-08-04 22:31:42 +0800

    移植到vue架構下

---
+++ vue-bidpage/src/components/signal.vue
@@ -0,0 +1,40 @@
+<template>
+  <article ref="Article">
+    <div><h2>防禦信號緒論</h2>
+      <p>防禦信號的內容，其實和叫牌制度沒什麼太大的關連。</p>
+      
+    </div>
+    <div><h2>防禦信號緒論</h2>
+      <p>防禦信號的內容，其實和叫牌制度沒什麼太大的關連。</p>
+      
+    </div>
+    <div><h2>防禦信號緒論</h2>
+      <p>防禦信號的內容，其實和叫牌制度沒什麼太大的關連。</p>
+      
+    </div>
+    <div><h2>防禦信號緒論</h2>
+      <p>防禦信號的內容，其實和叫牌制度沒什麼太大的關連。</p>
+      
+    </div>
+    <div><h2>防禦信號緒論</h2>
+      <p>防禦信號的內容，其實和叫牌制度沒什麼太大的關連。</p>
+      
+    </div>
+    <div><h2>防禦信號緒論</h2>
+      <p>防禦信號的內容，其實和叫牌制度沒什麼太大的關連。</p>
+      
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$root.replaceSuitSigns(this.$refs.Article);
+    this.$root.genTOC();
+  }
+}
+</script>
+
+<style>
+</style>
