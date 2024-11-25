5ceafc4ac5176df2e928334330722c8fe44aa112
C.H. Hsiao <hsiaosvideo@gmail.com>
2024-11-25 19:39:13 +0800

    New directory for precision club

---
+++ pages/precision/index.vue
@@ -0,0 +1,3 @@
+<template><h2>Precision Club</h2>
+<p>精準制，相關資料準備中</p>
+</template>
---
+++ pages/precision/precision1nt.vue
@@ -0,0 +1,48 @@
+<template>
+  <article ref="Article">
+      <div><h2>特殊牌形滿貫叫法</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>牌形</th>
+              <th>首次叫牌</th>
+              <th>第二次叫牌</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>(54)22</td>
+              <td>2!C</td>
+              <td>Smolen</td>
+            </tr>
+            <tr>
+              <td>(42)(52)</td>
+              <td>2!C</td>
+              <td>3m</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+  </article>
+  </template>
+
+<script setup lang="ts">
+import replaceSuitSigns from '~/modules/replaceSuitSigns';
+import genTOC from '~/modules/genTOC';
+import type { TocElement } from '~/type/type';
+
+const article = useTemplateRef<HTMLElement>('Article');
+const toc = useState<TocElement[]>('toc');
+
+const title = useState<string>('title');
+const topic = useState<string>('topic');
+
+onMounted(() => {
+  if (article.value !== null) {
+    replaceSuitSigns(article.value);
+  }
+  genTOC(toc);
+  topic.value = "Precision 1NT後續";
+  title.value = "Precision Club";
+});
+</script>
