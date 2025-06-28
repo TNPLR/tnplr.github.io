0a75f2f0c9a350f25d8ea0803b14775d44ccc09f
C.H. Hsiao <hsiaosvideo@gmail.com>
2025-06-28 16:21:35 +0800

    Fix bug

--- pages/precision/index.vue
+++ pages/precision/index.vue
@@ -1,3 +1,44 @@
-<template><h2>Precision Club</h2>
-<p>精準制，相關資料準備中</p>
+<template>
+<article ref="Article">
+    <div><h2>Precision 1!C</h2>
+    <p>本文參考《中華精準制》及Enhanced Precision書籍，準備一個以自然叫品為基礎的1!C後續。</p>
+    <div><h3>首次答叫</h3>
+        <table class="bidtable">
+            <tbody>
+                <tr>
+                    <th>1!C</th>
+                    <th>16點以上非平均牌或17點以上平均牌形，不含19-20平均牌形。</th>
+                </tr>
+                <tr>
+                    <td>1!D!</td>
+                    <td>0-7</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</article>
 </template>
+
+<script setup lang="ts">
+import replaceSuitSigns from '~/modules/replaceSuitSigns';
+import genTOC from '~/modules/genTOC';
+import type { TocElement } from '~/type/type';
+
+const article = useTemplateRef<HTMLElement>('Article');
+const toc = useState<TocElement[]>('toc');
+
+const title = useState<string>('title');
+const topic = useState<string>('topic');
+
+onMounted(() => {
+  if (article.value !== null) {
+    replaceSuitSigns(article.value);
+  }
+  genTOC(toc);
+  topic.value = "Precision";
+  title.value = "Precision Club";
+});
+</script>
+<style scoped>
+</style>
